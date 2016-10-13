f127c80a0623c3f1b73e10e83513c8d3dbc4d1bf
Nicholas Zuccarelli <s3546815@student.rmit.edu.au>
2016-10-13 15:49:25 +1100

    minor bug fixes

--- app.js
+++ app.js
@@ -45,9 +45,11 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  next();
 });
 
 // error handlers
