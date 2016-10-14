df6bab6027b3d6b3354a595e1f6a2111ac81cd6e
NICHOLAS <s3546815@student.rmit.edu.au>
2016-10-14 12:02:27 +1100

    comments hardcoded

--- models/Comment.js
+++ models/Comment.js
@@ -7,13 +7,11 @@ var Schema = mongoose.Schema;
 
 // create the user schema
 var commentSchema = new Schema({
-    title: String,
     body: String,
     movie_id: String,
     user_id: String, // link the user to the comment
     parent_id: String, // used if the comment is a reply
     created_at: Date,
-    updated_at: Date
 });
 
 
