6c99697fa843321c205a803c314ec9bc5cd9025c
avtar singh <s3530196@student.rmit.edu.au>
2016-10-14 18:08:04 +1100

    Displaying comments

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ var async = require('async');
 var credentials = require('../config/credentials.js');
 var Movies = require('../models/Movie');
 var Comments = require('../models/Comment');
+var Users = require('../models/User');
 
 function doCall(urlToCall, callback) {
   urllib.request(urlToCall, { wd: 'nodejs' }, function (err, data, response) {
@@ -65,6 +66,13 @@ router.get('/', function(req, res, next) {
     });
 });
 
+/*Get Comments for specific movie*/
+
+
+
+
+
+
 /* Individual post*/
 router.get('/post', function(req, res, next) {
   var id = req.query.id;
@@ -77,12 +85,67 @@ router.get('/post', function(req, res, next) {
     if(result.Response != "False") {
       title = result.Title + " - Mavericks Movie Blog";
       checkMovieExists(result.imdbID);
+
+        var found = false;
+        var arr = [];
+        var appender = [];
+        Comments.find({movie_id : result.imdbID}).exec(function(err, result){
+            if(err) {
+                throw err;
+            }
+            if(result.length < 1) {
+                found = false;
+            }
+            else {
+                found = true;
+            }
+            for(var temp in result){
+                if (result[temp][parent_id] == "-1"){
+                    var pusher = {};
+                    pusher['comment'] = result[temp];
+                    pusher['user'] = [];
+                    if(result[temp]['user_id'] != "-1") {
+                        // do a query to grab from users table result[temp]['user_id']
+                        Users.findOne({_id : result[temp]['user_id']}).exec(function(err,result1){
+                            pusher['user'].push(result1);
+                        });
+                    }
+                    pusher['replies'] = [];
+
+                    for(var r in result) {
+                        if(result[r]['parent_id'] == result[temp]['_id']) {
+                            var pusher2 = {};
+                            pusher2['comment'] = result[r];
+                            pusher2['user'] = [];
+                            if(result[r]['user_id'] != "-1") {
+                                Users.findOne({_id : result[r]['user_id']}).exec(function(err,result2){
+                                    pusher2['user'].push(result2);
+                                });
+                            }
+
+                            pusher['replies'].push(pusher2);
+                        }
+                    }
+
+                    arr.push(pusher);
+                }
+            }
+            for(var user in arr)
+            {
+
+                var message = result[user]['body'];
+            }
+
+
+        });
+
     }
     res.render('post', {
       title: title,
       movie: result,
       login: checkLoggedIn(req),
-      user: req.user
+      user: req.user,
+      comments:arr
     });
   });
 });
